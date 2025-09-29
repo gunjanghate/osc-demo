18b0d610eb1a6b082d0bf383c78b053d8ba0f185
gunjanghate <ghategunjan@gmail.com>
2025-09-29 17:18:49 +0530

    feat: adding test pages

---
+++ app/test1/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Test 1 Page
+    </div>
+  )
+}
+
+export default page
---
+++ app/test2/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Test 2 Page
+    </div>
+  )
+}
+
+export default page
